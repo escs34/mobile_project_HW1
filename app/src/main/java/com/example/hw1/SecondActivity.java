9ae7232ff6d0b8b2b1371022eb7d416a53f67b6b
Son Tae Seon <escs34@gmail.com>
2019-10-28 22:53:48 +0900

    UI of SecondActivity updated
     UI of SecondActivity(layout, button, background color) is implemented
    SecondActivity.java is just dummy code.

---
+++ app/src/main/java/com/example/hw1/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.hw1;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+        setTitle("Second 액티비티");
+
+    }
+
+}
